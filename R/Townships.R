0ed6e725d7b76ff1dd124f57a3f6ff6c829ff731
Michael Hahsler <michael@hahsler.net>
2022-07-03 16:54:44 -0500

    changed markup.

--- R/Townships.R
+++ R/Townships.R
@@ -9,11 +9,10 @@
 #' @docType data
 #' @format
 #'   A matrix with 16 0-1 variables (columns) indicating the presence
-#'   (\code{1}) or absence (\code{0}) of characteristics of townships
+#'   (`1`) or absence (`0`) of characteristics of townships
 #'   (rows).
 #' @references
-#'     Bertin, J. (1981): \emph{Graphics and Graphic Information Processing}.
-#'       Berlin, Walter de Gruyter.
+#' Bertin, J. (1981): _Graphics and Graphic Information Processing_. Berlin, Walter de Gruyter.
 #' @author Michael Hahsler
 #' @examples
 #' data("Townships")
@@ -21,7 +20,6 @@
 #' ## original data
 #' pimage(Townships)
 #' criterion(Townships)
-#'
 #'
 #' ## seriated data
 #' order <- seriate(Townships, method = "BEA", control = list(rep = 5))
