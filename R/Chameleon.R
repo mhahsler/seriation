20d58ab50dab951300ab48ff084dce88c0667b7a
Michael Hahsler <michael@hahsler.net>
2022-07-15 09:38:00 -0500

    removed broken link.

--- R/Chameleon.R
+++ R/Chameleon.R
@@ -1,8 +1,7 @@
 #' 2D Data Sets used for the CHAMELEON Clustering Algorithm
 #'
-#' Several 2D data sets used to evaluate the CHAMELEON clustering algorithm in
-#' the paper by Karypis et al (1999) and used by iVAT, an ordering-based tool
-#' to asses cluster tendency (Havens and Bezdek, 2012).
+#' Several 2D data sets created to evaluate the CHAMELEON clustering algorithm in
+#' the paper by Karypis et al (1999).
 #'
 #' @name Chameleon
 #' @aliases Chameleon chameleon chameleon_ds4 chameleon_ds5 chameleon_ds7
@@ -20,19 +19,16 @@
 #' Clustering Algorithm Using Dynamic Modeling, _IEEE Computer,_
 #' **32**(8): 68--75.
 #' \doi{10.1109/2.781637}
-#'
-#' Havens, T.C. and Bezdek, J.C. (2012): An Efficient Formulation of the
-#' Improved Visual Assessment of Cluster Tendency (iVAT) Algorithm, _IEEE
-#' Transactions on Knowledge and Data Engineering,_ **24**(5), 813--822.
-#' \doi{10.1109/TKDE.2011.33}
-#'
-#' @source The data was obtained from
-#' \url{http://glaros.dtc.umn.edu/gkhome/cluto/cluto/download}
 #' @keywords datasets
 #' @examples
 #' data(Chameleon)
 #'
 #' plot(chameleon_ds4, cex = .1)
+#' plot(chameleon_ds5, cex = .1)
+#' plot(chameleon_ds7, cex = .1)
+#' plot(chameleon_ds8, cex = .1)
 NULL
 
-
+# link does not work
+# @source The data was obtained from
+# \url{http://glaros.dtc.umn.edu/gkhome/cluto/cluto/download}
